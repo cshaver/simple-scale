4cc5759bf591a6c1f2656bb7fc2a18555a20aa84
Cristina Shaver <cristina.shaver@gmail.com>
2017-06-14 01:44:59 -0700

    feat(): Super basic functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,32 @@
-const Soundfont = require('soundfont-player');
+import '../scss/index.scss';
 
-// const aNote = document.getElementById('a');
-// const bNote = document.getElementById('b');
-// const cNote = document.getElementById('c');
-// const dNote = document.getElementById('d');
-// const eNote = document.getElementById('e');
-// const fNote = document.getElementById('f');
-// const gNote = document.getElementById('g');
+import Soundfont from 'soundfont-player';
+
+const notes = document.getElementById('notes');
 
 const ac = new AudioContext();
 
-// The first step is always create an instrument:
-Soundfont.instrument(ac, 'orchestral_harp', { soundfont: 'FluidR3_GM' }).then(function(clavinet) {
-  // Then you can play a note using names or midi numbers:
-  clavinet.play('C4');
-	clavinet.play('B4');
+var instrument;
+window.instrument = instrument;
 
-  // you can stop all playing notes
-  clavinet.stop();
-  // or stop only one
-  clavinet.play('B4').stop(ac.currentTime + 0.5);
-  // or pass a duration argument to `play`
-  clavinet.play('A4', ac.currentTime, {duration: 0.5});
-});
+// The first step is always create an instrument:
+Soundfont.instrument(ac, 'orchestral_harp', {soundfont: 'FluidR3_GM'}).then(
+	(harp) => {
+		instrument = harp;
+	}
+);
+
+initialize();
+
+function initialize() {
+	notes.addEventListener('click', event => {
+		if (event.target.classList.contains('note-button')) {
+			let button = event.target;
+			let note = button.name;
+
+			console.log(button, note);
+
+			instrument.play(note);
+		}
+	});
+}
--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,16 @@ module.exports = {
 		filename: 'index.built.js',
 		path: path.resolve(__dirname, 'dist/scripts')
 	},
+	module: {
+		rules: [{
+			test: /\.scss$/,
+			use: [{
+				loader: 'style-loader' // creates style nodes from JS strings
+			}, {
+				loader: 'css-loader' // translates CSS into CommonJS
+			}, {
+				loader: 'sass-loader' // compiles Sass to CSS
+			}]
+		}]
+	}
 };
