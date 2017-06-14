a1a1a0dacd6405ee16541ddec016c0ea5b7fe655
Cristina Shaver <cristina.shaver@gmail.com>
2017-06-14 00:04:47 -0700

    feat(): Play some rando notes

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Scale
+
+Something like https://www.youtube.com/watch?v=bkoz-t8cwxI&feature=youtu.be
---
+++ index.js
@@ -0,0 +1,23 @@
+// const aNote = document.getElementById('a');
+// const bNote = document.getElementById('b');
+// const cNote = document.getElementById('c');
+// const dNote = document.getElementById('d');
+// const eNote = document.getElementById('e');
+// const fNote = document.getElementById('f');
+// const gNote = document.getElementById('g');
+
+const ac = new AudioContext();
+
+// The first step is always create an instrument:
+Soundfont.instrument(ac, 'orchestral_harp', { soundfont: 'FluidR3_GM' }).then(function(clavinet) {
+  // Then you can play a note using names or midi numbers:
+  clavinet.play('C4');
+	clavinet.play('B4');
+
+  // you can stop all playing notes
+  clavinet.stop();
+  // or stop only one
+  clavinet.play('B4').stop(ac.currentTime + 0.5);
+  // or pass a duration argument to `play`
+  clavinet.play('A4', ac.currentTime, {duration: 0.5});
+});
